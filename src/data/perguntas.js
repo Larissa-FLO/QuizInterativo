6631525bb1021ab28725dad96424efd3277c8191
Larissa-FLO <larissa.arqueo@outlook.com>
2024-01-17 17:14:47 -0300

    quizGameofThrones

---
+++ src/data/perguntas.js
@@ -0,0 +1,124 @@
+export const Perguntas  = [
+    {
+        pergunta:'Qual o maior país de Westeros?',
+        nivel: "facil",
+        opcoesResposta:[
+            {resposta:'Vale', correta:false, alternativa:'A)'},
+            {resposta:'Dorne', correta:false, alternativa:'B)'},
+            {resposta:'Norte', correta:true, alternativa:'C)'},
+            {resposta:'Campina', correta:false, alternativa:'D)'},
+        ]
+    },
+    
+    {
+        pergunta:'Qual a Casa que passou a governar Westeros após a Rebelião?',
+        nivel: "facil",
+        opcoesResposta:[
+            {resposta:'Lannister', correta:false, alternativa:'A)'},
+            {resposta:'Baratheon', correta:true, alternativa:'B)'},
+            {resposta:'Stark', correta:false, alternativa:'C)'},
+            {resposta:'Targaryen', correta:false, alternativa:'D)'},
+        ]
+    },
+
+    {
+        pergunta:'Qual das Grandes Casas tem um leão dourado no brasão?',
+        nivel: "facil",
+        opcoesResposta:[
+            {resposta:'Lannister', correta:true, alternativa:'A)'},
+            {resposta:'Tyrell', correta:false, alternativa:'B)'},
+            {resposta:'Baratheon', correta:false, alternativa:'C)'},
+            {resposta:'Tully', correta:false, alternativa:'D)'},
+        ]
+    },
+    
+    {
+        pergunta:'Qual o lema da Casa Tully?',
+        nivel: "facil",
+        opcoesResposta:[
+            {resposta:'Ouça-me Rugir', correta:false, alternativa:'A)'},
+            {resposta:'Família, Dever e Honra', correta:true, alternativa:'B)'},
+            {resposta:'Nossa é a Fúria', correta:false, alternativa:'C)'},
+            {resposta:'Alto como a Honra', correta:false, alternativa:'D)'},
+        ]
+    },
+
+    {
+        pergunta:'Qual foi o único país que Aegon não conquistou?',
+        nivel: "medio",
+        opcoesResposta:[
+            {resposta:'Vale', correta:false, alternativa:'A)'},
+            {resposta:'Terras Ocidentais', correta:false, alternativa:'B)'},
+            {resposta:'Dorne', correta:true, alternativa:'C)'},
+            {resposta:'Terras da Tempestade', correta:false, alternativa:'D)'},
+        ]
+    },
+
+    {
+        pergunta:'Qual o nome da lendária espada de Eddard Stark?',
+        nivel: "medio",
+        opcoesResposta:[
+            {resposta:'Gelo', correta:true, alternativa:'A)'},
+            {resposta:'Neve', correta:false, alternativa:'B)'},
+            {resposta:'Garra de Lobo', correta:false, alternativa:'C)'},
+            {resposta:'Nevasca', correta:false, alternativa:'D)'},
+        ]
+    },
+
+    {
+        pergunta:'Quais raças habitavam Westeros antes da invasão dos Primeiros Homens?',
+        nivel: "dificil",
+        opcoesResposta:[
+            {resposta:'Os Outros', correta:false, alternativa:'A)'},
+            {resposta:'Os Gigantes e os Outros', correta:false, alternativa:'B)'},
+            {resposta:'Os Filhos da Floresta e os Gigantes', correta:true, alternativa:'C)'},
+            {resposta:'Os Filhos da Floresta e os Outros', correta:false, alternativa:'D)'},
+            
+        ]
+    },
+
+    {
+        pergunta:'O Príncipe Rhaegar Targaryen notoriamente ganhou o Torneio em Harrenhal, mas quem ele derrotou na disputa final?',
+        nivel: "dificil",
+        opcoesResposta:[
+            {resposta:'Brandon Stark', correta:false, alternativa:'A)'},
+            {resposta:'Sor Barristan Selmy', correta:true, alternativa:'B)'},
+            {resposta:'Sor Duncan', correta:false, alternativa:'C)'},
+            {resposta:'Jaime Lannister', correta:false, alternativa:'D)'},
+        ]
+    },
+    
+    {
+        pergunta:'O que fez os Targaryens deixarem Valíria e se mudarem para a Pedra do Dragão?',
+        nivel: "dificil",
+        opcoesResposta:[
+            {resposta:'Foram banidos acusados de traição', correta:false, alternativa:'A)'},
+            {resposta:'Partiram em busca de mais glória', correta:false, alternativa:'B)'},
+            {resposta:'pela falta de influência, partiram para conquistar Westeros', correta:false, alternativa:'C)'},
+            {resposta:'Fugiram após um Targaryen ter visões do cataclismo que aconteceria', correta:true, alternativa:'D)'},
+        ]
+    },
+
+    {
+        pergunta:'Antes de se estabelecer com seu povo em Dorne, quantos navios a lenda diz que a Rainha Nymeria usou para transportar os Roinares sobreviventes depois que eles foram derrotados na batalha pela Cidade Franca de Valíria?',
+        nivel: "dificil",
+        opcoesResposta:[
+            {resposta:'10.000', correta:true, alternativa:'A)'},
+            {resposta:'1.000', correta:false, alternativa:'B)'},
+            {resposta:'100', correta:false, alternativa:'C)'},
+            {resposta:'100.000', correta:false, alternativa:'D)'},
+        ]
+    },
+    
+    {
+        pergunta:'Qual dessas espadas não é de Aço Valiriano',
+        nivel: "dificil",
+        opcoesResposta:[
+            {resposta:'Garralonga', correta:false, alternativa:'A)'},
+            {resposta:'Gelo', correta:false, alternativa:'B)'},
+            {resposta:'Veneno do Coração', correta:false, alternativa:'C)'},
+            {resposta:'Alvorada', correta:true, alternativa:'D)'},
+        ]
+    },
+
+    ]
